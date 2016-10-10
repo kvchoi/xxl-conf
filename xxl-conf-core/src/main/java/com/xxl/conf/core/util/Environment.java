90a3d44c3f9771b47510ec42738ce8dee9718ccb
cairf <cairongfu.crf@alibaba-inc.com>
2016-10-10 11:57:00 +0800

    demo

--- xxl-conf-core/src/main/java/com/xxl/conf/core/util/Environment.java
+++ xxl-conf-core/src/main/java/com/xxl/conf/core/util/Environment.java
@@ -2,33 +2,36 @@ package com.xxl.conf.core.util;
 
 import java.util.Properties;
 
+import org.springframework.util.StringUtils;
+
 /**
  * 环境基类
+ * 
  * @author xuxueli 2015-8-28 10:37:43
  */
 public class Environment {
 
-	/**
-	 * conf data path in zk
+    /**
+     * conf data path in zk
      */
-	public static final String CONF_DATA_PATH = "/xxl-conf";
+    public static final String CONF_DATA_PATH = "/xxl-conf";
 
-	/**
-	 * zk config file
+    /**
+     * zk config file
      */
-	private static final String ZK_ADDRESS_FILE = "/data/webapps/xxl-conf.properties";
+    private static final String ZK_ADDRESS_FILE = "/data/webapps/xxl-conf.properties";
 
-	/**
-	 * zk address
+    /**
+     * zk address
      */
-	public static final String ZK_ADDRESS;		// zk地址：格式	ip1:port,ip2:port,ip3:port
-	static {
-		Properties prop = PropertiesUtil.loadFileProperties(ZK_ADDRESS_FILE);
-		ZK_ADDRESS = PropertiesUtil.getString(prop, "zkserver");
-	}
+    public static final String ZK_ADDRESS; // zk地址：格式 ip1:port,ip2:port,ip3:port
+    static {
+        Properties prop = PropertiesUtil.loadFileProperties(ZK_ADDRESS_FILE);
+        String zkserver = PropertiesUtil.getString(prop, "zkserver");
+        ZK_ADDRESS = StringUtils.isEmpty(zkserver) ? "127.0.0.1:2181" : zkserver;
+    }
 
-	public static void main(String[] args) {
-		System.out.println(ZK_ADDRESS);
-	}
+    public static void main(String[] args) {
+        System.out.println(ZK_ADDRESS);
+    }
 }
-
